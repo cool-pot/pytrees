d5d2025a9f394a8cc3ceccf6ef3eb9bb82883c8a
cool-pot <601746479@qq.com>
2018-05-21 09:29:43 -0400

    add setup

--- pytrees/__init__.py
+++ pytrees/__init__.py
@@ -4,3 +4,11 @@ from pytrees.BinarySearchTree import BinarySearchTree
 from pytrees.IntervalTree import IntervalTree
 from pytrees.Trie import Trie
 
+__version__ = "0.0.1"
+__short_description__ = "AVL Tree, Interval Tree, Trie and More. All kinds of Trees implemented in python3. "
+__license__ = "MIT"
+__author__ = "Yi Zhou"
+__author_email__ = "601746479@qq.com"
+__maintainer__ = "Yi Zhou"
+__maintainer_email__ = "601746479@qq.com"
+__github_username__ = "cool-pot"
